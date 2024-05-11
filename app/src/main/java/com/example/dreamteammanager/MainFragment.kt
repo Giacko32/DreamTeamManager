77855ee72f3ec59a9e3343d2891de16db06ae7de
Gioele2002 <gioele.fontanini1@gmail.com>
2024-05-11 11:36:03 +0200

    Isa

--- app/src/main/java/com/example/dreamteammanager/MainFragment.kt
+++ app/src/main/java/com/example/dreamteammanager/MainFragment.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.example.dreamteammanager.auth.RecuperoCredenzialiFragment
+import com.example.dreamteammanager.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
 
+    lateinit var binding: FragmentMainBinding
 
 
     override fun onCreateView(
@@ -17,6 +23,21 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonCreateLeague.setOnClickListener{
+            val fragmentManager = (context as AppCompatActivity).supportFragmentManager
+            fragmentManager.commit {
+                setReorderingAllowed(true)
+                replace<CreaLegaFragment>(R.id.fragment_container)
+                addToBackStack("login_to_recupero")
+            }
+        }
+
     }
 
 
