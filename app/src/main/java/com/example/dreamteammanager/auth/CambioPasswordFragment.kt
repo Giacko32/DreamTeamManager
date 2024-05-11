bebaf7fd5125d8da2ab7b7826b7d4b2ec325fa0a
Gianvito12 <gianvitocognata261@gmail.com>
2024-05-11 12:43:03 +0200

    image

--- app/src/main/java/com/example/dreamteammanager/auth/CambioPasswordFragment.kt
+++ app/src/main/java/com/example/dreamteammanager/auth/CambioPasswordFragment.kt
@@ -6,19 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.dreamteammanager.R
+import com.example.dreamteammanager.databinding.FragmentCambioPasswordBinding
+import com.example.dreamteammanager.databinding.FragmentRecuperoCredenzialiBinding
 
 class CambioPasswordFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    lateinit var binding: FragmentCambioPasswordBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cambio_password, container, false)
+        binding = FragmentCambioPasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.confermaButton.setOnClickListener {
     }
 
 }
