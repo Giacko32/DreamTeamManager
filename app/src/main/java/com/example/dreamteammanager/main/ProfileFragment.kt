d5877c13bc389a5628a64282ecc5fd9e4523ee7c
Giacko32 <giacomolombardo67@gmail.com>
2024-05-11 11:44:13 +0200

    icona

--- app/src/main/java/com/example/dreamteammanager/main/ProfileFragment.kt
+++ app/src/main/java/com/example/dreamteammanager/main/ProfileFragment.kt
@@ -1,13 +1,16 @@
-package com.example.dreamteammanager.auth
+package com.example.dreamteammanager.main
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.dreamteammanager.R
+import com.example.dreamteammanager.databinding.FragmentProfileBinding
 
-class CambioPasswordFragment : Fragment() {
+class ProfileFragment : Fragment() {
+
+    lateinit var binding: FragmentProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +21,8 @@ class CambioPasswordFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cambio_password, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
+
 
 }
