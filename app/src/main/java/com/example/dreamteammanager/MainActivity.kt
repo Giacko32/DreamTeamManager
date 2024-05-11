ab712a5036badedd4141a7cb3525686371b38c9d
Giacko32 <giacomolombardo67@gmail.com>
2024-05-11 10:13:13 +0200

    icona

--- app/src/main/java/com/example/dreamteammanager/MainActivity.kt
+++ app/src/main/java/com/example/dreamteammanager/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.dreamteammanager
 
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.MenuRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.add
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,5 +30,27 @@ class MainActivity : AppCompatActivity() {
             setReorderingAllowed(true)
             add<MainFragment>(R.id.fragmentContainerView)
         }
+
     }
+
+    private fun showMenu(v: View, @MenuRes menuRes: Int) {
+        val popup = PopupMenu(v.context!!, v)
+        popup.menuInflater.inflate(menuRes, popup.menu)
+        popup.setOnMenuItemClickListener {item: MenuItem? ->
+            when(item!!.itemId) {
+                R.id.option_1 -> {
+
+                }
+            }
+            true
+        }
+        popup.setOnDismissListener {
+            // Respond to popup being dismissed.
+        }
+        popup.show()
+    }
+
+
 }
+
+
