a42c27b670aa19af7395390480ee895bd30fc085
Giacko32 <giacomolombardo67@gmail.com>
2024-05-11 12:23:27 +0200

    icona

--- app/src/main/java/com/example/dreamteammanager/auth/AccessActivity.kt
+++ app/src/main/java/com/example/dreamteammanager/auth/AccessActivity.kt
@@ -3,6 +3,7 @@ package com.example.dreamteammanager.auth
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.add
@@ -11,6 +12,7 @@ import com.example.dreamteammanager.R
 
 class AccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_access)
@@ -19,6 +21,7 @@ class AccessActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
 
         supportFragmentManager.commit {
             setReorderingAllowed(true)
