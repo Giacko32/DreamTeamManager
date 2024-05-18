16232f4e8f09a4f9501722a6deb8e910df34b0a5
Giacko32 <giacomolombardo67@gmail.com>
2024-05-18 10:37:11 +0200

    icona

--- app/src/main/java/com/example/dreamteammanager/lega/LegaView.kt
+++ app/src/main/java/com/example/dreamteammanager/lega/LegaView.kt
@@ -1,5 +1,8 @@
 package com.example.dreamteammanager.lega
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,10 +14,11 @@ import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dreamteammanager.R
+import com.example.dreamteammanager.classi.Competizione
 import com.example.dreamteammanager.classi.Utente
 import com.example.dreamteammanager.databinding.FragmentLegaviewBinding
-import com.example.dreamteammanager.databinding.FragmentListaCompetizioniBinding
 
 
 class LegaView : Fragment() {
@@ -27,13 +31,21 @@ class LegaView : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val listaCompetizioni = Dialog(requireActivity())
+
         binding.CompetizioniButton.setOnClickListener{
-            parentFragmentManager.commit {
-                setReorderingAllowed(true)
-                setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                replace<ListaCompetizioni>(R.id.legafragmentcontainer)
-                addToBackStack("competizioni")
+            listaCompetizioni.setContentView(R.layout.dialog_lista_competizioni)
+            listaCompetizioni.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            val rv = listaCompetizioni.findViewById<RecyclerView>(R.id.recviewcomp)
+            val lista = ArrayList<Competizione>(0)
+            for(i in 1..10)
+            {
+                lista.add(Competizione(0, "competizione a caso", "motogp", 0))
             }
+            val adapter = CompetizioniAdapter(lista)
+            rv.layoutManager = LinearLayoutManager(context)
+            rv.adapter = adapter
+            listaCompetizioni.show()
         }
 
         val lista = ArrayList<Utente>()
