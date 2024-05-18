7e6dd06cfec4d058b2a955053fa81b26590268a7
Giacko32 <giacomolombardo67@gmail.com>
2024-05-18 11:27:54 +0200

    icona

--- app/src/main/java/com/example/dreamteammanager/main/MainFragment.kt
+++ app/src/main/java/com/example/dreamteammanager/main/MainFragment.kt
@@ -1,11 +1,15 @@
 package com.example.dreamteammanager.main
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -30,12 +34,12 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val creaLegaDialog = Dialog(requireActivity())
+
         binding.creaNuovaLegaButton.setOnClickListener {
-            parentFragmentManager.commit {
-                setReorderingAllowed(true)
-                replace<CreaLegaFragment>(R.id.fragmentContainerView)
-                addToBackStack("creaLega")
-            }
+            creaLegaDialog.setContentView(R.layout.dialog_crea_lega)
+            creaLegaDialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            creaLegaDialog.show()
         }
 
         val lega= Lega(1,"LEGA",100,1)
