b84b8cd998c606cd8c74b4d04232f711ee0c92bc
Gianvito12 <gianvitocognata261@gmail.com>
2024-05-25 15:05:56 +0200

    image

--- app/src/main/java/com/example/dreamteammanager/retrofit/UserAPI.kt
+++ app/src/main/java/com/example/dreamteammanager/retrofit/UserAPI.kt
@@ -95,6 +95,8 @@ interface UserAPI {
     fun getpart(@Path("idcomp")idcomp: Int):Call<JsonArray>
     @GET("pwm/getgiornatedacalcolare/{idcomp}")
     fun getgiornatedacalcolare(@Path("idcomp")idcomp: Int):Call<JsonArray>
+    @POST("pwm/calcolagiornata")
+    fun calcolagiornata(@Body compezionegiornata: JsonObject):Call<JsonObject>
 
 
     companion object {
--- app/src/main/java/com/example/dreamteammanager/viewmodel/CompetizioniVM.kt
+++ app/src/main/java/com/example/dreamteammanager/viewmodel/CompetizioniVM.kt
@@ -6,50 +6,57 @@ import androidx.lifecycle.ViewModel
 import com.example.dreamteammanager.classi.Competizione
 import com.example.dreamteammanager.classi.Utente
 import com.example.dreamteammanager.retrofit.Client
+import com.google.gson.Gson
 import com.google.gson.JsonArray
+import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-class CompetizioniVM: ViewModel(){
+class CompetizioniVM : ViewModel() {
 
     private var _competizione = MutableLiveData<Competizione?>()
     val competizione: LiveData<Competizione?>
         get() = _competizione
+
     init {
         _competizione.value = null
     }
-    private val _idamm= MutableLiveData<Int?>()
+
+    private val _idamm = MutableLiveData<Int?>()
     val idamm: LiveData<Int?>
         get() = _idamm
+
     init {
         _idamm.value = null
     }
-    fun setidamm(id: Int){
+
+    fun setidamm(id: Int) {
         _idamm.value = id
     }
 
     private val _sport = MutableLiveData<String?>()
     val sport: LiveData<String?>
         get() = _sport
+
     init {
         _sport.value = null
     }
 
-    public fun setCompetizione( c: Competizione)
-    {
+    public fun setCompetizione(c: Competizione) {
         _competizione.value = c
     }
 
-    public fun setSport( s: String)
-    {
+    public fun setSport(s: String) {
         _sport.value = s
-        _competizione.value?.sport=s
+        _competizione.value?.sport = s
     }
-    public fun setnome( s: String){
-        _competizione.value?.nome=s
+
+    public fun setnome(s: String) {
+        _competizione.value?.nome = s
     }
+
     private val _partecipanti = MutableLiveData<ArrayList<Utente>>()
     val partecipanti: LiveData<ArrayList<Utente>> = _partecipanti
 
@@ -68,8 +75,9 @@ class CompetizioniVM: ViewModel(){
     fun resetscaricando() {
         _scaricando.value = null
     }
-    fun getpartecipanti(){
-        _scaricando.value=true
+
+    fun getpartecipanti() {
+        _scaricando.value = true
         _partecipanti.value?.clear()
         Client.retrofit.getpart(competizione.value!!.id).enqueue(
             object : Callback<JsonArray> {
@@ -92,4 +100,98 @@ class CompetizioniVM: ViewModel(){
             }
         )
     }
+
+    private val _prendendo = MutableLiveData<Boolean?>()
+    val prendendo: LiveData<Boolean?>
+        get() = _prendendo
+
+    init {
+        _prendendo.value = null
+    }
+
+    fun resetprendendo() {
+        _prendendo.value = null
+    }
+
+    private val _giornatedacalcolare = MutableLiveData<ArrayList<Giornata>>()
+    val giornatedacalcolare: LiveData<ArrayList<Giornata>> = _giornatedacalcolare
+
+    init {
+        _giornatedacalcolare.value = arrayListOf()
+    }
+
+    fun getgiornatedacalcolare() {
+        _prendendo.value = true
+        _giornatedacalcolare.value?.clear()
+        Client.retrofit.getgiornatedacalcolare(competizione.value!!.id).enqueue(
+            object : Callback<JsonArray> {
+                override fun onResponse(
+                    call: Call<JsonArray>, response:
+                    Response<JsonArray>
+                ) {
+                    if (response.isSuccessful) {
+                        _giornatedacalcolare.value = parseJsonToArrayInt(response.body().toString())
+                        _prendendo.value = false
+                    }
+                }
+
+                override fun onFailure(
+                    call: Call<JsonArray>?, t:
+                    Throwable?
+                ) {
+                    _prendendo.value = false
+                }
+            }
+        )
+    }
+
+    private val _calcolando = MutableLiveData<Boolean?>()
+    val calcolando: LiveData<Boolean?>
+        get() = _calcolando
+
+    init {
+        _calcolando.value = null
+    }
+
+    fun resetcalcolando() {
+        _calcolando.value = null
+    }
+
+    fun calcolagiornata(giornata: Int) {
+        _calcolando.value = true
+        val body = Gson().fromJson(
+            parseModelToJson(CompGiorn(competizione.value!!, giornata)),
+            JsonObject::class.java
+        )
+        Client.retrofit.calcolagiornata(body).enqueue(
+            object : Callback<JsonObject> {
+                override fun onResponse(
+                    call: Call<JsonObject>, response:
+                    Response<JsonObject>
+                ) {
+                    if (response.isSuccessful) {
+                        _calcolando.value = false
+                    }
+                }
+
+                override fun onFailure(
+                    call: Call<JsonObject>?, t:
+                    Throwable?
+                ) {
+                    _calcolando.value = false
+                }
+            }
+        )
+    }
 }
+
+    fun parseJsonToArrayInt(jsonString: String): ArrayList<Giornata> {
+        val gson = Gson()
+        return gson.fromJson(
+            jsonString,
+            object : com.google.gson.reflect.TypeToken<ArrayList<Giornata>>() {}.type
+        )
+    }
+
+    class CompGiorn(val competizione: Competizione, val giornata: Int)
+    class Giornata(val giornata: Int)
